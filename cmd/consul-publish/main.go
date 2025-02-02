00de48e816f6ce57be9b66cfc914463954c8d0e4
Igor Kulkov <iakulkov@avito.ru>
2025-02-02 15:22:14 +0300

    cleanup

--- cmd/consul-publish/main.go
+++ cmd/consul-publish/main.go
@@ -2,49 +2,64 @@ package main
 
 import (
 	"context"
+	"log/slog"
 	"os"
 	"os/signal"
-	"sync"
+	"strings"
 	"syscall"
-	"time"
 
-	"github.com/jfk9w/consul-publish/internal/api"
-	"github.com/jfk9w/consul-publish/internal/log"
-	"github.com/jfk9w/consul-publish/internal/publish"
-	"github.com/jfk9w/consul-publish/internal/target"
-	"github.com/jfk9w/consul-publish/internal/target/hosts"
-	"github.com/jfk9w/consul-publish/internal/target/porkbun"
-
-	"github.com/AlekSi/pointer"
-	"github.com/coreos/go-systemd/daemon"
+	capi "github.com/hashicorp/consul/api"
 	"github.com/jfk9w-go/confi"
-	"github.com/pkg/errors"
-	"go.uber.org/multierr"
+
+	"github.com/jfk9w/consul-publish/internal/consul"
+	"github.com/jfk9w/consul-publish/internal/listeners/caddy"
+	"github.com/jfk9w/consul-publish/internal/listeners/hosts"
 )
 
 type Config struct {
-	Dump *struct {
-		Schema bool `yaml:"schema,omitempty" doc:"Dump configuration schema in YAML."`
-		Values bool `yaml:"values,omitempty" doc:"Dump configuration values in YAML."`
-	} `yaml:"dump,omitempty" doc:"Dump info in stdout."`
-
 	Address string `yaml:"address,omitempty" doc:"Consul address" default:"127.0.0.1:8500"`
 	Token   string `yaml:"token" doc:"Consul token"`
-
-	Once     bool          `yaml:"once,omitempty" doc:"Execute once and exit"`
-	Interval time.Duration `yaml:"interval,omitempty" doc:"Consul watch interval" default:"1m"`
-	Domain   api.Domain    `yaml:"domain" doc:"Domain settings"`
 
 	Hosts struct {
 		Enabled      bool `yaml:"enabled,omitempty" doc:"Enable hosts target"`
 		hosts.Config `yaml:",inline"`
 	} `yaml:"hosts,omitempty" doc:"Hosts target settings"`
 
-	Porkbun struct {
-		Enabled        bool   `yaml:"enabled,omitempty" doc:"Enable Porkbun target"`
-		CredentialsKey string `yaml:"credentialsKey,omitempty" doc:"Consul key for Porkbun credentials"`
-		porkbun.Config `yaml:",inline"`
-	} `yaml:"porkbun,omitempty" doc:"Porkbun target settings"`
+	Caddy struct {
+		Enabled      bool `yaml:"enabled,omitempty" doc:"Enable caddy target"`
+		caddy.Config `yaml:",inline"`
+	} `yaml:"caddy,omitempty" doc:"Caddy target settings"`
+}
+
+func newConsulClient(address, token string) (*capi.Client, error) {
+	cfg := capi.DefaultConfig()
+	cfg.Address = address
+	cfg.Token = token
+	return capi.NewClient(cfg)
+}
+
+func newLogger() *slog.Logger {
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == "source" {
+				if source, ok := a.Value.Any().(*slog.Source); ok {
+					fn := source.Function
+					slash := strings.LastIndex(fn, "/")
+					if slash > 0 {
+						fn = fn[slash+1:]
+					}
+
+					return slog.String("caller", fn)
+				}
+			}
+
+			return a
+		},
+	})
+
+	return slog.New(handler)
 }
 
 var exit = []os.Signal{
@@ -56,99 +71,33 @@ var exit = []os.Signal{
 }
 
 func main() {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Error(context.Background(), "panic", "desc", r)
-			os.Exit(1)
-		}
-	}()
-
 	ctx, cancel := signal.NotifyContext(context.Background(), exit...)
 	defer cancel()
 
-	var work sync.WaitGroup
-	defer work.Wait()
-
-	work.Add(1)
-	go func() {
-		defer work.Done()
-		<-ctx.Done()
-		notify(daemon.SdNotifyStopping)
-	}()
-
-	if err := run(ctx); err != nil {
-		for _, err := range multierr.Errors(err) {
-			log.Error(ctx, err.Error())
-		}
-
-		os.Exit(1)
-	}
-}
-
-func run(ctx context.Context) error {
-	cfg, schema, err := confi.Get[Config](ctx, "consul-publish")
+	cfg, _, err := confi.Get[Config](ctx, "consul-publish")
 	if err != nil {
-		return errors.Wrap(err, "read config")
-	}
-
-	if pointer.Get(cfg.Dump).Schema {
-		dump(schema, confi.YAML)
-		return nil
-	}
-
-	if pointer.Get(cfg.Dump).Values {
-		cfg.Dump = nil
-		dump(cfg, confi.YAML)
-		return nil
-	}
-
-	defer log.Info(ctx, "shutdown")
-
-	consul, err := api.NewConsul(cfg.Address, cfg.Token)
-	if err != nil {
-		return errors.Wrap(err, "create consul client")
-	}
-
-	interval := cfg.Interval
-	if cfg.Once {
-		interval = -1
-	}
-
-	targets := make(target.Registry).
-		Add(cfg.Hosts.Enabled, "hosts", target.Hosts(cfg.Hosts.Config)).
-		Add(cfg.Porkbun.Enabled, "porkbun", target.Porkbun(cfg.Porkbun.Config, consul, cfg.Porkbun.CredentialsKey))
-
-	for nodes, err := range publish.Watch(ctx, consul, interval) {
-		if err != nil || nodes == nil {
-			return errors.Wrap(err, "watch consul")
-		}
-
-		local, err := consul.Local(ctx)
-		if err != nil {
-			return errors.Wrap(err, "get local node")
-		}
-
-		ctx := log.With(ctx, "local", local.Name)
-		if err := publish.Run(ctx, cfg.Domain, local, nodes, targets); err != nil {
-			return err
-		}
-
-		ready()
-	}
-
-	return nil
-}
-
-func dump(value any, codec confi.Codec) {
-	if err := codec.Marshal(value, os.Stdout); err != nil {
 		panic(err)
 	}
-}
 
-var ready = sync.OnceFunc(func() { notify(daemon.SdNotifyReady) })
+	client, err := newConsulClient(cfg.Address, cfg.Token)
+	if err != nil {
+		panic(err)
+	}
 
-func notify(state string) {
-	if _, err := daemon.SdNotify(false, state); err != nil {
-		log.Warn(context.Background(), "failed to notify systemd", err)
+	slog.SetDefault(newLogger())
+	defer slog.Info("shutdown")
+
+	var listeners []consul.Listener
+
+	if cfg.Hosts.Enabled {
+		listeners = append(listeners, hosts.New(cfg.Hosts.Config))
+	}
+
+	if cfg.Caddy.Enabled {
+		listeners = append(listeners, caddy.New(cfg.Caddy.Config))
+	}
+
+	if err := consul.Watch(ctx, client, listeners...); err != nil {
+		panic(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,20 +3,17 @@ module github.com/jfk9w/consul-publish
 go 1.23.4
 
 require (
-	github.com/AlekSi/pointer v1.2.0
-	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
 	github.com/hashicorp/consul/api v1.31.0
 	github.com/jfk9w-go/confi v0.0.5
 	github.com/pkg/errors v0.9.1
 	github.com/tiendc/go-deepcopy v1.2.0
-	go.uber.org/multierr v1.11.0
 	golang.org/x/sync v0.10.0
 )
 
 require (
+	github.com/AlekSi/pointer v1.2.0 // indirect
 	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/fatih/color v1.18.0 // indirect
-	github.com/godbus/dbus v4.1.0+incompatible // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-hclog v1.6.3 // indirect
