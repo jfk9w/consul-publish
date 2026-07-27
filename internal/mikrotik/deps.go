f6f9caa054419bf879b4112c9511fb0d6456c995
jfk9w <ja.mudopu@sonc.top>
2026-07-27 23:32:26 +0300

    mise tasks

--- internal/mikrotik/deps.go
+++ internal/mikrotik/deps.go
@@ -2,7 +2,7 @@ package mikrotik
 
 import "net/http"
 
-//go:generate mockgen -source=deps.go -destination=mock_test.go -package=mikrotik_test
+//go:generate mockgen -source=deps.go -destination=mocks_test.go -package=mikrotik_test
 
 // HTTPClient is the interface used by Client to send HTTP requests.
 type HTTPClient interface {
